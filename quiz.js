1afd848aac90c835d2458fcca379d151f61d0dae
LucasMcL <LMcLaughlin09@gmail.com>
2016-12-12 14:22:58 -0600

    Outlined functions specified in instructions

---
+++ quiz.js
@@ -0,0 +1,58 @@
+////////////////
+///Objectives///
+////////////////
+
+var color = "firebrick";
+
+// Loads in the JSON and stores in a variable
+// Executed upon JSON file loading
+function loadInventory() {
+
+}
+
+// Function to populate the page
+// Executed after loadIventory?
+function populatePage() {
+
+}
+
+// Executed at the very beginning
+// Adds all the event listeners
+function activateEvents() {
+  // Listen for JSON file to load
+  // Listen for click event on card
+  // Listen for description to be updated (so border can go away)
+
+}
+
+// Executed when car description is changed (enter pressed)
+// (Optional) Executed when focus is lost?
+function resetBorder() {
+
+}
+
+// Executed whenever a click occurs anywhere?
+  // Checks to see if that element is the card div
+  // Checks to see if any of the parent elements are the card div
+// Takes a color variable and manually changes CSS?
+  // Would be better if it changed class and color was changed in CSS file
+  // But...I will follow instructions
+function changeCardBorder(node, color) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
